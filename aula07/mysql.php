24398a201746bb8f712076b94f5f63810cb45d42
Lucas Marques <lucasmarques73@hotmail.com>
2018-03-07 18:59:54 -0300

    finalizando mysql

--- aula07/mysql.php
+++ aula07/mysql.php
@@ -7,4 +7,17 @@ $db	  = "aula07";
 
 $conexao = mysqli_connect($host,$user,$pass,$db);
 
-mysqli_query($conexao,"INSERT INTO posts (usuario_id,post) VALUES (4,'Escrevendo mais posts');");
+// $query = "INSERT INTO posts (usuario_id,post) VALUES (4,'Escrevendo mais posts');"
+
+// mysqli_query($conexao,$query);
+
+$query = "SELECT * FROM posts WHERE usuario_id = 4";
+
+$result = mysqli_query($conexao,$query);
+
+echo "<pre>";
+while ($row = mysqli_fetch_assoc($result)) {
+	echo "Post: {$row['id']} <br>";
+	echo $row['post'];
+	echo "<hr>";
+}
